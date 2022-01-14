e832142ebc909b87021879b4cb341b224dfaa4ba
Jodie <70372661+JodieMc@users.noreply.github.com>
2022-01-14 19:26:10 +0000

    Create Colors.java

---
+++ Colors.java
@@ -0,0 +1,20 @@
+package ie.gmit.dip;
+
+/**
+ * @author Josephine Mc Crann
+ * @version 1.0
+ * @since 1.8 
+ * Enums are special classes which represent constants that can be
+ *        grouped together. Colors are unchangeable variables here.
+ */
+
+public enum Colors {
+	RED, 
+	BLUE, 
+	GREEN, 
+	YELLOW, 
+	BLACK, 
+	BROWN, 
+	PURPLE
+
+}
