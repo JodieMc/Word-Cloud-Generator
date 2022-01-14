29cc1a684c18129301484ddf3474da0e58b83bf4
Jodie <70372661+JodieMc@users.noreply.github.com>
2022-01-14 19:27:58 +0000

    Create Fonts.java

---
+++ Fonts.java
@@ -0,0 +1,21 @@
+package ie.gmit.dip;
+
+/**
+ * @author Josephine Mc Crann
+ * @version 1.0
+ * @since 1.8 
+ * Enums are special classes which represent constants that can be
+ *        grouped together. Fonts fall into this category in this application.
+ */
+
+public enum Fonts {
+	Arial,
+	Impact,
+	Tahoma,
+	Didot,
+	Georgia,
+	Verdana,
+	Courier,
+	Monaco,
+	Luminari
+}
